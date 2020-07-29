8d9d8aa3f5c58dd05ada36edad03249c6aa4a833
jorgechauxjr <chauxjorge@hotmail.com>
2020-07-29 17:33:10 -0500

    Fix Betty Style to 0-binary_tree_node.c

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,12 +11,12 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-	
+
 	newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
 		return (NULL);
-	
+
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
