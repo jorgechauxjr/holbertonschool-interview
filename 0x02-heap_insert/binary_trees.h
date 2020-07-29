5888589751bb66e5173df7bac5c19bd9e22faeca
jorgechauxjr <chauxjorge@hotmail.com>
2020-07-29 17:55:33 -0500

    Fix binary_trees.h L14 remove typedef and L20 remove binary_tree_t

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,14 +12,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
-
+};
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s heap_t;
 
@@ -27,6 +27,6 @@ void binary_tree_print(const binary_tree_t *);
 /*0. New node*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* 1. Heap - Insert */
-heap_t *heap_insert(heap_t **root, int value);
+
 
 #endif /* _BINARY_TREES_H_ */
