c18e5fb621c73a14bb9a18c89da0486b817dac3d
jorgechauxjr <chauxjorge@hotmail.com>
2020-08-10 10:01:21 -0500

    Add 0-minoperations.py

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-def copyPaste(h):
-    return h + h
 
 def minOperations(n):
-    h, cont = 1, 0
-
-    while(h < n):
-        cont = cont + 1
-        h = copyPaste(h)
-    return cont * 2
+    if n <= 1:
+        return 0
+    num, div, numOfOperations = n, 2, 0
+    
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
+    
