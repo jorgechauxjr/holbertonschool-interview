9acbe3f500408d3056818b0eb41be50e94fe47f1
jorgechauxjr <chauxjorge@hotmail.com>
2020-08-18 17:04:35 -0500

    Update sanpiles.h and Add grid_change and grid_ok to 0-sandpiles.c

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,6 +2,9 @@
 #define SANDPILES_H
 
 void grid_addition(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif /* SANDPILES_H */
